d26be40876702967e1e25ebbfc32b079baea0cad
perlancar (on pc-home) <perlancar@gmail.com>
2017-04-16 08:46:20 +0700

    Initial commit

---
+++ lib/Devel/End/PrintINCVersion.pm
@@ -0,0 +1,36 @@
+package Devel::End::PrintINCVersion;
+
+# DATE
+# VERSION
+
+use strict;
+use warnings;
+
+use ExtUtils::MakeMaker;
+
+END {
+    print "Versions of files in %INC:\n";
+    for my $k (sort keys %INC) {
+        my $path = $INC{$k};
+        print "  $k ($path): ";
+        if (-f $path) {
+            my $v = MM->parse_version($path);
+            print $v if defined $v;
+        }
+        print "\n";
+    }
+}
+
+1;
+# ABSTRACT: Print versions of files (modules) listed in %INC
+
+=head1 SYNOPSIS
+
+ % perl -MDevel::End::PrintINCVersion -e'...'
+
+
+=head1 DESCRIPTION
+
+After loading this module, when program ends, versions of files (modules) listed
+in C<%INC> will be printed to STDOUT. The versions are extracted using
+L<ExtUtils::MakeMaker>'s C<parse_version>.
